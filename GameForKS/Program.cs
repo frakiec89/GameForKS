1c293949c07e836cb867344083ae2f1d27e21e4b
РИШАТ Ахтямов <rishat-rus@mail.ru>
2024-01-29 15:02:16 +0400

    fd

--- GameForKS/Program.cs
+++ GameForKS/Program.cs
@@ -1,23 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
 using GameForKS;
 Console.WriteLine("Угадай число");
-
-Console.WriteLine("Введите Ваше имя");
-
+Console.WriteLine("Введите Ваше имя:");
 string name = Console.ReadLine();
+Console.WriteLine("Укажите ваш статус:");
+string description = Console.ReadLine();
 
-Console.WriteLine("Расскажите о себе");
-
-string d = Console.ReadLine();
-
-
-GameService  game = new GameService(name , d  , 0 , 50 );
+GameService  game = new GameService(name , description, 0 , 50 );
 
 while (true)
 {
     Console.WriteLine("Введите число");
     int x = Convert.ToInt32(Console.ReadLine());
-
     Console.WriteLine(  game.RaundGo(x));
 }
